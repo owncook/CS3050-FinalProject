631dd8ab445405d9a96b06d4343cd0230357a416
owencook <oacook@uvm.edu>
2024-10-25 10:35:38 -0400

    created trapezoid and enemy classes

---
+++ trapezoid.py
@@ -0,0 +1,33 @@
+import arcade
+import math
+
+
+
+
+
+
+class trapezoid():
+
+    row1 = arcade.SpriteList()
+    row2 = arcade.SpriteList()
+    row3 = arcade.SpriteList()
+    row4 = arcade.SpriteList()
+    row5 = arcade.SpriteList()
+
+
+    def __init__(self):
+        # For loop to populate 2x10 2x8 and 1x4
+
+
+        # enemy = SwoopingEnemy("sources/enemies/json.jpeg", scale=1, target_x=target_x, target_y=target_y)
+
+        # # append the enemy to a list of enemies
+        # self.row1.append(enemy)
+
+    
+
+    # set home position
+
+    # draw sprite
+
+
