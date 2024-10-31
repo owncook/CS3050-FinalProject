38a5992ad30c0a670da53f06bf670afdc6bd71ce
cwodarz <cwodarz@uvm.edu>
2024-10-31 11:13:19 -0400

    added attack_interval constant

--- constant.py
+++ constant.py
@@ -14,9 +14,13 @@ ENEMY_SCALE = 0.25
 ENEMY_SPACING_X = 80  # Horizontal spacing between enemies
 ENEMY_SPACING_Y = 100  # Vertical spacing between rows
 ENEMY_SPAWN_INTERVAL = 3
+ATTACK_INTERAL = 4
 
 # --- Stars ---
 STAR_COUNT = 100
 STAR_SPEED = 2
 TWINKLE_SPEED = 0.1
+
+
+
 
