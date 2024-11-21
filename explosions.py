7ed4e3acbfb96004feb6e07e228f9d2f28964f29
cwodarz <cwodarz@uvm.edu>
2024-11-21 11:18:13 -0500

    cleaning up documentation for explosions.py

--- explosions.py
+++ explosions.py
@@ -9,19 +9,19 @@ arcade.load_font("sources/fonts/emulogic-font/Emulogic-zrEw.ttf")
 
 
 class Smoke(arcade.SpriteCircle):
-    """ This represents a puff of smoke """
+    """ This class represents a puff of smoke that appears behind the exploded sprite. """
     def __init__(self, size):
         super().__init__(size, arcade.color.LIGHT_GRAY, soft=True)
         self.change_y = SMOKE_RISE_RATE
         self.scale = SMOKE_START_SCALE
 
     def update(self):
-        """ Update this particle """
+        """ Update the smoke particle """
         if self.alpha <= PARTICLE_FADE_RATE:
-            # Remove faded out particles
+            # Remove faded out particles.
             self.remove_from_sprite_lists()
         else:
-            # Update values
+            # Update values.
             self.alpha -= SMOKE_FADE_RATE
             self.center_x += self.change_x
             self.center_y += self.change_y
@@ -29,47 +29,46 @@ class Smoke(arcade.SpriteCircle):
 
 
 class Particle(arcade.SpriteCircle):
-    """ Explosion particle """
+    """ This class represents an explosion particle that flies out of the center of the exploded sprite. """
     def __init__(self, my_list):
-        # Choose a random color
+        # Choose a random color.
         color = random.choice(PARTICLE_COLORS)
 
         # Make the particle
         super().__init__(PARTICLE_RADIUS, color)
 
-        # Track normal particle texture, so we can 'flip' when we sparkle.
+        # Track normal particle texture, this allows us to flip on sparkle.
         self.normal_texture = self.texture
 
-        # Keep track of the list we are in, so we can add a smoke trail
+        # Keep track of the list we are in, this allows us to add a smoke trail.
         self.my_list = my_list
 
-        # Set direction/speed
+        # Set direction/speed of the explosion particles.
         speed = random.random() * PARTICLE_SPEED_RANGE + PARTICLE_MIN_SPEED
         direction = random.randrange(360)
         self.change_x = math.sin(math.radians(direction)) * speed
         self.change_y = math.cos(math.radians(direction)) * speed
 
-        # Track original alpha. Used as part of 'sparkle' where we temp set the
-        # alpha back to 255
+        # Track original alpha. Used as part of sparkle where alpha is temporarily set back to 255.
         self.my_alpha = 255
 
-        # What list do we add smoke particles to?
+        # Add smoke particles to the list we are currently in. 
         self.my_list = my_list
 
     def update(self):
-        """ Update the particle """
+        """ Update the explosion particle """
         if self.my_alpha <= PARTICLE_FADE_RATE:
-            # Faded out, remove
+            # Remove the particle if it has faded out. 
             self.remove_from_sprite_lists()
         else:
-            # Update
+            # Update particle position and opacity.
             self.my_alpha -= PARTICLE_FADE_RATE
             self.alpha = self.my_alpha
             self.center_x += self.change_x
             self.center_y += self.change_y
             self.change_y -= PARTICLE_GRAVITY
 
-            # Should we sparkle this?
+            # Randomly sparkle the explosion.
             if random.random() <= PARTICLE_SPARKLE_CHANCE:
                 self.alpha = 255
                 self.texture = arcade.make_circle_texture(int(self.width),
@@ -77,7 +76,7 @@ class Particle(arcade.SpriteCircle):
             else:
                 self.texture = self.normal_texture
 
-            # Leave a smoke particle?
+            # Randomly leave smoke behind after the explosion.
             if random.random() <= SMOKE_CHANCE:
                 smoke = Smoke(5)
                 smoke.position = self.position
