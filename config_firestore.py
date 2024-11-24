5b8c1827509356c61330c67e12ae2a44df70ae35
Tuckers15 <taschulz@uvm.edu>
2024-11-23 20:02:22 -0500

    adding documentation

--- config_firestore.py
+++ config_firestore.py
@@ -2,6 +2,10 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+'''
+config_firestore.py locates our firebase key and initializes the firebase application and database objects which are imported into database.py
+'''
+
 # Use a service account.
 cred = credentials.Certificate('keys/cs-3050-final-project-538f66db8997.json')
 
